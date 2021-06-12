1ae632581090980652638eb89d5f37e7d7a22d6e
Mick Coelho <mick.coelho@b-reel.com>
2021-06-11 17:37:39 -0700

    remove locales

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-module.exports = {
-  i18n: {
-    locales: ['en', 'fr'],
-    defaultLocale: 'en',
-  },
-};
+module.exports = {};
