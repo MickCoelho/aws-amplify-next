1f620eac23d9cf6812b3db24321ef65224bb0a36
Mick Coelho <mick.coelho@b-reel.com>
2021-06-11 17:49:36 -0700

    Disabel automatic local detection

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-module.exports = {};
+module.exports = {
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    localeDetection: false,
+  },
+};
